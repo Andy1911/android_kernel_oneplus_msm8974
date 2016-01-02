b3575b932895a6d5fec1cbb8d90b1a7525beef5d
Andy1911 <aanand.godbole17@gmail.com>
2016-01-02 21:16:51 +0530

    Revert "time: Kill xtime_lock, replacing it with jiffies_lock"
    This reverts commit ab8ead09c8c8c385fe9e2963e3bad23dace63e7b.

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -24,6 +24,13 @@
 
 static struct timekeeper timekeeper;
 
+/*
+ * This read-write spinlock protects us from races in SMP while
+ * playing with xtime.
+ */
+__cacheline_aligned_in_smp DEFINE_SEQLOCK(xtime_lock);
+
+
 /* flag for if timekeeping is suspended */
 int __read_mostly timekeeping_suspended;
 
@@ -1296,7 +1303,9 @@ struct timespec get_monotonic_coarse(void)
 }
 
 /*
- * Must hold jiffies_lock
+ * The 64-bit jiffies value is not atomic - you MUST NOT read it
+ * without sampling the sequence number in xtime_lock.
+ * jiffies is defined in the linker script...
  */
 void do_timer(unsigned long ticks)
 {
@@ -1385,7 +1394,7 @@ EXPORT_SYMBOL_GPL(ktime_get_monotonic_offset);
  */
 void xtime_update(unsigned long ticks)
 {
-	write_seqlock(&jiffies_lock);
+	write_seqlock(&xtime_lock);
 	do_timer(ticks);
-	write_sequnlock(&jiffies_lock);
+	write_sequnlock(&xtime_lock);
 }
